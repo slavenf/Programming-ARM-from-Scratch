a532b502246ea7c1697a810de9cfb4126aeb1b4c
Slaven Falandys <sfalandys@yahoo.com>
2023-02-09 17:11:51 +0100

    Update

--- 05_peripheral_register_systematization/STM32F411xE.h
+++ 05_peripheral_register_systematization/STM32F411xE.h
@@ -24,34 +24,35 @@ struct RCC_t
     volatile uint32_t CIR;          // RCC clock interrupt register.                                Address offset: 0x0C
     volatile uint32_t AHB1RSTR;     // RCC AHB1 peripheral reset register.                          Address offset: 0x10
     volatile uint32_t AHB2RSTR;     // RCC AHB2 peripheral reset register.                          Address offset: 0x14
-    volatile uint32_t RESERVED0;    // Reserved.                                                    Address offset: 0x1C
+    volatile uint32_t RESERVED0;    // Reserved.                                                    Address offset: 0x18
+    volatile uint32_t RESERVED1;    // Reserved.                                                    Address offset: 0x1C
     volatile uint32_t APB1RSTR;     // RCC APB1 peripheral reset register.                          Address offset: 0x20
     volatile uint32_t APB2RSTR;     // RCC APB2 peripheral reset register.                          Address offset: 0x24
-    volatile uint32_t RESERVED1;    // Reserved.                                                    Address offset: 0x28
-    volatile uint32_t RESERVED2;    // Reserved.                                                    Address offset: 0x2C
+    volatile uint32_t RESERVED2;    // Reserved.                                                    Address offset: 0x28
+    volatile uint32_t RESERVED3;    // Reserved.                                                    Address offset: 0x2C
     volatile uint32_t AHB1ENR;      // RCC AHB1 peripheral clock enable register.                   Address offset: 0x30
     volatile uint32_t AHB2ENR;      // RCC AHB2 peripheral clock enable register.                   Address offset: 0x34
-    volatile uint32_t RESERVED3;    // Reserved.                                                    Address offset: 0x38
-    volatile uint32_t RESERVED4;    // Reserved.                                                    Address offset: 0x3C
+    volatile uint32_t RESERVED4;    // Reserved.                                                    Address offset: 0x38
+    volatile uint32_t RESERVED5;    // Reserved.                                                    Address offset: 0x3C
     volatile uint32_t APB1ENR;      // RCC APB1 peripheral clock enable register.                   Address offset: 0x40
     volatile uint32_t APB2ENR;      // RCC APB2 peripheral clock enable register.                   Address offset: 0x44
-    volatile uint32_t RESERVED5;    // Reserved.                                                    Address offset: 0x48
-    volatile uint32_t RESERVED6;    // Reserved.                                                    Address offset: 0x4C
+    volatile uint32_t RESERVED6;    // Reserved.                                                    Address offset: 0x48
+    volatile uint32_t RESERVED7;    // Reserved.                                                    Address offset: 0x4C
     volatile uint32_t AHB1LPENR;    // RCC AHB1 peripheral clock enable in low power mode register. Address offset: 0x50
     volatile uint32_t AHB2LPENR;    // RCC AHB2 peripheral clock enable in low power mode register. Address offset: 0x54
-    volatile uint32_t RESERVED7;    // Reserved.                                                    Address offset: 0x58
-    volatile uint32_t RESERVED8;    // Reserved.                                                    Address offset: 0x5C
+    volatile uint32_t RESERVED8;    // Reserved.                                                    Address offset: 0x58
+    volatile uint32_t RESERVED9;    // Reserved.                                                    Address offset: 0x5C
     volatile uint32_t APB1LPENR;    // RCC APB1 peripheral clock enable in low power mode register. Address offset: 0x60
     volatile uint32_t APB2LPENR;    // RCC APB2 peripheral clock enable in low power mode register. Address offset: 0x64
-    volatile uint32_t RESERVED9;    // Reserved.                                                    Address offset: 0x68
-    volatile uint32_t RESERVED10;   // Reserved.                                                    Address offset: 0x6C
+    volatile uint32_t RESERVED10;   // Reserved.                                                    Address offset: 0x68
+    volatile uint32_t RESERVED11;   // Reserved.                                                    Address offset: 0x6C
     volatile uint32_t BDCR;         // RCC Backup domain control register.                          Address offset: 0x70
     volatile uint32_t CSR;          // RCC clock control & status register.                         Address offset: 0x74
-    volatile uint32_t RESERVED11;   // Reserved.                                                    Address offset: 0x78
-    volatile uint32_t RESERVED12;   // Reserved.                                                    Address offset: 0x7C
+    volatile uint32_t RESERVED12;   // Reserved.                                                    Address offset: 0x78
+    volatile uint32_t RESERVED13;   // Reserved.                                                    Address offset: 0x7C
     volatile uint32_t SSCGR;        // RCC spread spectrum clock generation register.               Address offset: 0x80
     volatile uint32_t PLLI2SCFGR;   // RCC PLLI2S configuration register.                           Address offset: 0x84
-    volatile uint32_t RESERVED13;   // Reserved.                                                    Address offset: 0x88
+    volatile uint32_t RESERVED14;   // Reserved.                                                    Address offset: 0x88
     volatile uint32_t DCKCFGR;      // RCC Dedicated Clocks Configuration Register.                 Address offset: 0x8C
 };
 
