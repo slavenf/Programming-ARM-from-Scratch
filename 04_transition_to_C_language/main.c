1baf4c1929a4de3c672884ae3dd6188a79903282
Slaven Falandys <sfalandys@yahoo.com>
2023-03-22 01:14:50 +0100

    Update

--- 04_transition_to_C_language/main.c
+++ 04_transition_to_C_language/main.c
@@ -1,19 +1,23 @@
 // Peripheral base addresses
-#define RCC_BASE    0x40023800
-#define GPIOC_BASE  0x40020800
+#define GPIOC_BASE      0x40020800
+#define RCC_BASE        0x40023800
 
-// RCC_AHB1ENR register
-#define RCC_AHB1ENR                 (RCC_BASE + 0x30)
+// GPIOC peripheral register addresses
+#define GPIOC_MODER     (GPIOC_BASE + 0x0)
+#define GPIOC_ODR       (GPIOC_BASE + 0x14)
+
+// RCC peripheral register addresses
+#define RCC_AHB1ENR     (RCC_BASE + 0x30)
+
+// Bit definitions for GPIO_MODER register
+#define GPIO_MODER_MODER13_MASK     (3 << 26)
+#define GPIO_MODER_MODER13_OUTPUT   (1 << 26)
+
+// Bit definitions for GPIO_ODR register
+#define GPIO_ODR_ODR13              (1 << 13)
+
+// Bit definitions for RCC_AHB1ENR register
 #define RCC_AHB1ENR_GPIOCEN         (1 << 2)
-
-// GPIOC_MODER register
-#define GPIOC_MODER                 (GPIOC_BASE + 0x0)
-#define GPIOC_MODER_MODER13_MASK    (3 << 26)
-#define GPIOC_MODER_MODER13_OUTPUT  (1 << 26)
-
-// GPIOC_ODR register
-#define GPIOC_ODR                   (GPIOC_BASE + 0x14)
-#define GPIOC_ODR_ODR13             (1 << 13)
 
 // Bit manipulation macros
 #define BIT_SET(register, mask)     ((register) |= (mask))
@@ -37,13 +41,13 @@ int main()
     BIT_SET( REG(RCC_AHB1ENR), RCC_AHB1ENR_GPIOCEN );
 
     // Set GPIOC pin 13 mode to output
-    BIT_CLEAR( REG(GPIOC_MODER), GPIOC_MODER_MODER13_MASK );
-    BIT_SET( REG(GPIOC_MODER), GPIOC_MODER_MODER13_OUTPUT );
+    BIT_CLEAR( REG(GPIOC_MODER), GPIO_MODER_MODER13_MASK );
+    BIT_SET( REG(GPIOC_MODER), GPIO_MODER_MODER13_OUTPUT );
 
     while (1)
     {
         // Toggle GPIOC pin 13
-        BIT_TOGGLE( REG(GPIOC_ODR), GPIOC_ODR_ODR13 );
+        BIT_TOGGLE( REG(GPIOC_ODR), GPIO_ODR_ODR13 );
         delay(500000);
     }
 }
